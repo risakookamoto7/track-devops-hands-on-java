93cc302be3b41fc1f5fda7f7baf0d1068bb8c6de
risakookamoto7 <risako.okamoto@tcs.com>
2026-06-12 13:15:04 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,8 +1,8 @@
 package com.example.handson;
 
 public class Calculator {
-  
-  public int add(int a, int b) {
-    return a + b;
-  }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
