2872a96854af48f7fedbdeecee2782a7b72288bb
risakookamoto7 <risako.okamoto@tcs.com>
2026-06-12 13:49:16 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -5,4 +5,17 @@ public class Calculator {
     public double add(double a, double b) {
         return a + b;
     }
+
+    public double add(double a, double b, double c) {
+        validateRange(a);
+        validateRange(b);
+        validateRange(c);
+        return a + b + c;
+    }
+
+    private void validateRange(double value) {
+        if (value &lt; 0 || value &gt; 10) {
+            throw new IllegalArgumentException("value must be between 0 and 10");
+        }
+    }
 }
